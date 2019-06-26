71212145ec02a350b0e531e7f1d3752e0a732885
strikertanmay <tanmaymittal0@gmail.com>
2019-06-27 01:30:19 +0530

    clone added for testing

--- index.js
+++ index.js
@@ -40,85 +40,84 @@ function loadTemplate(templateName, contexts) {
     );
 }
 
-function echo(event) {
+function echo(event1) {
+    if (event1.op === "INSERT") {
+        responseBody = `New user ${event1.data.new.id} inserted, with data: ${
+            event1.data.new.name
+            }`;
 
-    if (event === event1) {
-        let responseBody = "";
-
-        if (event1.op === "INSERT") {
-            responseBody = `New user ${event1.data.new.id} inserted, with data: ${
-                event1.data.new.name
-                }`;
-
-            loadTemplate("welcome", [event1.data])
-                .then(results => {
-                    return Promise.all(
-                        results.map(result => {
-                            sendEmail({
-                                to: result.context.new.email,
-                                from: "hello@cynthesize.co",
-                                subject: result.email.subject,
-                                html: result.email.html,
-                                text: result.email.text
-                            });
-                        })
-                    );
-                })
-                .catch(e => {
-                    console.log("Error Found: ", e);
-                });
-        } else if (event1.op === "UPDATE") {
-            responseBody = `User ${event1.data.new.id} updated, with data: ${
-                event1.data.new.name
-                }`;
-        } else if (event1.op === "DELETE") {
-            responseBody = `User ${event1.data.old.id} deleted, with data: ${
-                event1.data.old.name
-                }`;
-        }
-
-        return responseBody;
+        loadTemplate("welcome", [event1.data])
+            .then(results => {
+                return Promise.all(
+                    results.map(result => {
+                        sendEmail({
+                            to: result.context.new.email,
+                            from: "hello@cynthesize.co",
+                            subject: result.email.subject,
+                            html: result.email.html,
+                            text: result.email.text
+                        });
+                    })
+                );
+            })
+            .catch(e => {
+                console.log("Error Found: ", e);
+            });
+    } else if (event1.op === "UPDATE") {
+        responseBody = `User ${event1.data.new.id} updated, with data: ${
+            event1.data.new.name
+            }`;
+    } else if (event1.op === "DELETE") {
+        responseBody = `User ${event1.data.old.id} deleted, with data: ${
+            event1.data.old.name
+            }`;
     }
 
-    else if (event === event2) {
-        let responseBody = "";
-
-        if (event2.op === "INSERT") {
-            responseBody = `New project ${event2.data.new.id} inserted, with data: ${
-                event2.data.new.project_name
-                }`;
-
-            loadTemplate("Add_Project", [event2.session_variables])
-                .then(results => {
-                    return Promise.all(
-                        results.map(result => {
-                            sendEmail({
-                                to: result.context.x - hasura - email,
-                                from: "hello@cynthesize.co",
-                                subject: result.email.subject,
-                                html: result.email.html,
-                                text: result.email.text
-                            });
-                        })
-                    );
-                })
-                .catch(e => {
-                    console.log("Error Found: ", e);
-                });
-        } else if (event2.op === "UPDATE") {
-            responseBody = `User ${event2.data.new.id} updated, with data: ${
-                event2.data.new.project_name
-                }`;
-        } else if (event2.op === "DELETE") {
-            responseBody = `User ${event2.data.old.id} deleted, with data: ${
-                event2.data.old.project_name
-                }`;
-        }
-
-        return responseBody;
-    }
-
+    return responseBody;
 }
+
+function echo(event2){
+    let responseBody = "";
+
+    if (event2.op === "INSERT") {
+        responseBody = `New project ${event2.data.new.id} inserted, with data: ${
+            event2.data.new.project_name
+            }`;
+
+        loadTemplate("Add_Project", [event2.session_variables])
+            .then(results => {
+                return Promise.all(
+                    results.map(result => {
+                        sendEmail({
+                            to: result.context.x - hasura - email,
+                            from: "hello@cynthesize.co",
+                            subject: result.email.subject,
+                            html: result.email.html,
+                            text: result.email.text
+                        });
+                    })
+                );
+            })
+            .catch(e => {
+                console.log("Error Found: ", e);
+            });
+    } else if (event2.op === "UPDATE") {
+        responseBody = `User ${event2.data.new.id} updated, with data: ${
+            event2.data.new.project_name
+            }`;
+    } else if (event2.op === "DELETE") {
+        responseBody = `User ${event2.data.old.id} deleted, with data: ${
+            event2.data.old.project_name
+            }`;
+    }
+
+    return responseBody;
+}
+   
+        
+    
+
+
 
 
 app.post("/", function (req, res) {
